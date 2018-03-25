0b8af10540f60e27e34107ddc619e368a8c43b28
jbvincey <jbvincey@deezer.com>
2018-03-25 18:39:56 +0200

    update sample app

--- app/src/main/java/com/jbvincey/nestedradiobuttonsample/MainActivity.java
+++ app/src/main/java/com/jbvincey/nestedradiobuttonsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.jbvincey.nestedradiobuttonsample;
 
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getSupportActionBar().setElevation(0);
+        }
 
         ViewPager viewPager = findViewById(R.id.nested_sample_viewpager);
         viewPager.setAdapter(new SamplePagerAdapter(this));
